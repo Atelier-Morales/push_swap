074e3723eb734a334af792e8fea5bf392eb8fb55
Atelier-Morales <fmorales@student.42.fr>
2015-10-21 15:32:16 +0200

    added mergesort function

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fmorales <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/21 11:12:03 by fmorales          #+#    #+#             */
-/*   Updated: 2015/10/21 14:12:41 by fmorales         ###   ########.fr       */
+/*   Updated: 2015/10/21 14:52:03 by fmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,13 @@ int	main(int ac, char **av)
 	pile_b = NULL;
 	len = ac - 1;
 	if (ac < 2)
-	{
-		ft_putstr("\n");
 		return (0);
-	}
 	pile_a = create_pile(len, av + 1);
 	initialize_list(&pile_b);
 	if (pile_a)
 	{
 		push_swap(pile_a, pile_b);
-		print_list(pile_a);
+		//print_list(pile_a);
 	}
 	return (0);
 }
